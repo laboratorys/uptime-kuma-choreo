4aa62bb4f302ef642ec2d78ca115db914589311a
laboratorys <359916450@qq.com>
2025-07-15 17:17:30 +0800

    test3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-nohup /app/backup2gh &
-sleep 5
-retry_count=0
-max_retries=30
-while [ $retry_count -lt $max_retries ]; do
-    if [ -f "/app/restore.lock" ]; then
-        echo "$(date "+%Y-%m-%d %H:%M:%S") Waiting for restore from github..."
-        sleep 5
-        ((retry_count++))
-    else
-        break
-    fi
-done
-echo "$(date "+%Y-%m-%d %H:%M:%S") Starting uptime-kuma server..."
-node server/server.js
+/app/backup2gh
+##sleep 5
+#retry_count=0
+#max_retries=30
+#while [ $retry_count -lt $max_retries ]; do
+#    if [ -f "/app/restore.lock" ]; then
+#        echo "$(date "+%Y-%m-%d %H:%M:%S") Waiting for restore from github..."
+#        sleep 5
+#        ((retry_count++))
+#    else
+#        break
+#    fi
+#done
+#echo "$(date "+%Y-%m-%d %H:%M:%S") Starting uptime-kuma server..."
+#node server/server.js
